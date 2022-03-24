6235dfb0709a7055e8fdfb77ecf8e6316a1a16f7
0542sei <102280971+0542sei@users.noreply.github.com>
2022-03-24 15:58:05 +0900

    test

---
+++ test.js
@@ -0,0 +1,12 @@
+//기존 자바스크립트의 함수 사용 방법
+function add(first,second){
+    return first+second;
+}
+
+//ES6의 화살표 함수 사용 방법
+
+var add=(first,secoknd)=>{
+    return first+second;
+}
+
+var add=(first,second)=>first+second;
